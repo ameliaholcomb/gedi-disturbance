b7395dd86919f16d236200dd2949cd831f19a2a7
Amelia <amelia.holcomb@gmail.com>
2024-02-19 12:30:51 +0000

    [feat/fix] improve coincidence query performance.
    1. Use the new filtered shots table
    2. Use geometry::geography to ensure only a single index is used
    3. Include a query option for a non-spark version of the query (delegate
    parallelism to postgis only -- make sure that postgis parallel worker
    flags are set accordingly.)

--- src/processing/coincidence_query.py
+++ src/processing/coincidence_query.py
@@ -1,30 +1,26 @@
 import geopandas as gpd
 from src.data import spark_postgis
+from gedidb.database.gedidb_schema import Shots
 
 
 COLUMNS = [
     "shot_number",
     "geometry",
-    "agbd",
-    "absolute_time",
-    "beam_type",
-    "sensitivity",
-    "agbd_se",
 ]
+
+TABLE_NAME = "filtered_l2ab_l4a_shots"
 
 
 def get_columns(identifier: str) -> str:
     return ", ".join(
         [
-            f"{identifier}.{column} as  {identifier}_{column}"
-            for column in COLUMNS
+            f"{identifier}.{column} as {identifier}_{column}"
+            for column in Shots.metadata.tables[TABLE_NAME].columns.keys()
         ]
     )
 
 
 def get_query(region):
-    # TODO(amelia): Remove granule_name query constraint
-    # once the GEDI db has uniform versioning.
     return f"""
     (
     WITH FilteredTable AS NOT MATERIALIZED (
@@ -36,15 +32,32 @@ def get_query(region):
           MOD(MOD(hashint4(
             (TRUNC(ST_X(geometry) / 2) * 360 + TRUNC(ST_Y(geometry) / 2))::int
             ), 1000) + 1000, 1000) AS geo_partition
-        FROM level_4a 
+        FROM {TABLE_NAME} 
         WHERE 1=1
-            AND ST_Intersects(geometry, {spark_postgis.sqlify_geometry(region)})
-            AND degrade_flag = 0
-            AND l2_quality_flag = 1
+            AND ST_Intersects(geometry::geography, {spark_postgis.sqlify_geometry(region)})
             AND l4_quality_flag = 1
-            AND granule_name ~ '.*01_V002\.h5'
     )
     SELECT {get_columns('t1')}, {get_columns('t2')}, t1.geo_partition
+    FROM FilteredTable AS t1 JOIN FilteredTable AS t2
+        ON 1=1
+        AND ST_DWITHIN(t1.geometry::geography, t2.geometry::geography, 40, FALSE)
+        AND t1.shot_number < t2.shot_number
+    ) AS coincident_shots
+    """
+
+
+def get_query_nopartition(region):
+    return f"""
+    SELECT * FROM (
+    WITH FilteredTable AS NOT MATERIALIZED (
+        SELECT 
+          *
+        FROM {TABLE_NAME} 
+        WHERE 1=1
+            AND ST_Intersects(geometry::geography, {spark_postgis.sqlify_geometry(region)})
+            AND l4_quality_flag = 1
+    )
+    SELECT {get_columns('t1')}, {get_columns('t2')}
     FROM FilteredTable AS t1 JOIN FilteredTable AS t2
         ON 1=1
         AND ST_DWITHIN(t1.geometry::geography, t2.geometry::geography, 40, FALSE)
@@ -57,6 +70,5 @@ opts = {
     "numPartitions": 56,
     "partitionColumn": "geo_partition",
     "lowerBound": 0,
-    "upperBound": 1000,
+    "upperBound": 2000,
 }
-
